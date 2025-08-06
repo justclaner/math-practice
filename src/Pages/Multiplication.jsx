4f98fa3f15c31e65d8c8c4685f2f18e46da5ad34
= <justclaner@gmail.com>
2025-08-06 19:43:13 -0400

    added multiple difficulties

--- src/Pages/Multiplication.jsx
+++ src/Pages/Multiplication.jsx
@@ -19,7 +19,15 @@ const Multiplication = () => {
 
     const [maxStreak, setMaxStreak] = useState(0);
 
-    const millisecondsForEachQuestion = 5000;
+    const [secondsPerQuestion, setSecondsPerQuestion] = useState(5);
+
+    const [difficulty, setDifficulty] = useState(1);
+
+    const [minFactor1, setMinFactor1] = useState(0);
+    const [maxFactor1, setMaxFactor1] = useState(10);
+
+    const [minFactor2, setMinFactor2] = useState(0);
+    const [maxFactor2, setMaxFactor2] = useState(10);
 
     useEffect(() => {
         newQuestion();
@@ -30,12 +38,82 @@ const Multiplication = () => {
     }
 
     useEffect(() => {
+        console.log(difficulty)
+        switch (difficulty) {
+            case 1:
+                setSecondsPerQuestion(5);
+
+                setMinFactor1(0);
+                setMaxFactor1(10);
+
+                setMinFactor2(0);
+                setMaxFactor2(10);
+                break;
+            case 2:
+                setSecondsPerQuestion(15);
+
+                setMinFactor1(10);
+                setMaxFactor1(99);
+
+                setMinFactor2(0);
+                setMaxFactor2(10);
+                break;
+            case 3:
+                setSecondsPerQuestion(30);
+
+                setMinFactor1(10);
+                setMaxFactor1(99);
+
+                setMinFactor2(10);
+                setMaxFactor2(99);
+                break;
+            case 4:
+                setSecondsPerQuestion(45);
+
+                setMinFactor1(100);
+                setMaxFactor1(999);
+
+                setMinFactor2(10);
+                setMaxFactor2(99);
+                break;
+            case 5:
+                setSecondsPerQuestion(60);
+
+                setMinFactor1(100);
+                setMaxFactor1(999);
+
+                setMinFactor2(100);
+                setMaxFactor2(999);
+                break;
+            case 6:
+                setSecondsPerQuestion(100);
+
+                setMinFactor1(1000);
+                setMaxFactor1(9999);
+
+                setMinFactor2(1000);
+                setMaxFactor2(9999);
+                break;
+        }
+    }, [difficulty])
+
+    useEffect(() => {
+        setCorrect(0);
+        setMaxStreak(0);
+        setTotalCorrect(0);
+        setTotalQuestions(0);
+        setFeedback("");
+        setIncorrect(0);
+        newQuestion();
+    }, [maxFactor1, maxFactor2, minFactor1, minFactor2])
+
+    useEffect(() => {
         setMaxStreak(Math.max(maxStreak, correct));
     }, [correct])
 
     useEffect(() => {
         //console.log(stopwatch);
-        if (stopwatch >= millisecondsForEachQuestion) {
+        if (stopwatch >= secondsPerQuestion) {
             outOfTime();
         }
     }, [stopwatch])
@@ -43,7 +121,7 @@ const Multiplication = () => {
     useEffect(() => {
         const interval = setInterval(() => {
             const date = new Date();
-            setStopwatch(date.getTime() - startTime);
+            setStopwatch((date.getTime() - startTime) / 1000);
         }, 10);
 
         return () => clearInterval(interval);
@@ -73,8 +151,15 @@ const Multiplication = () => {
         setStartTime(date.getTime());
 
         setAnswer("");
-        let a = Math.floor(Math.random() * 11);
-        let b = Math.floor(Math.random() * 11);
+        let a = Math.floor(Math.random() * (maxFactor1 - minFactor1 + 1) + minFactor1);
+        let b = Math.floor(Math.random() * (maxFactor2 - minFactor2 + 1) + minFactor2);
+
+        //switch them randomly
+        if (Math.random() < 0.5) {
+            a ^= b;
+            b ^= a;
+            a ^= b;
+        }
         setFactor1(a);
         setFactor2(b);
         setProduct(a * b);
@@ -107,6 +192,21 @@ const Multiplication = () => {
         }}
         >
         <div className="text-3xl text-center">Multiplication Practice</div>
+        <div className="flex flex-row gap-3">
+            <div className="text-xl">Difficulty: </div>
+            <select name="" id="" className="border-2 border-black px-2 py-1 rounded-md"
+            onChange={(e) => {
+                setDifficulty(Number(e.target.value));
+            }}
+            >
+                <option value={1}>1) Times Table</option>
+                <option value={2}>2) 2-digit by 1-digit</option>
+                <option value={3}>3) 2-digit by 2-digit</option>
+                <option value={4}>4) 3-digit by 2-digit</option>
+                <option value={5}>5) 3-digit by 3-digit</option>
+                <option value={6}>6) 4-digit by 4-digit</option>
+            </select>
+        </div>
         <div className="text-center text-3xl">
             <InlineMath math={`${factor1}\\cdot${factor2}`}></InlineMath>
         </div>
@@ -124,7 +224,7 @@ const Multiplication = () => {
                 answerQuestion();
             }}>Enter</button>
         </div>
-        <div className="text-3xl">{`${Math.max(((5000 - stopwatch) / 1000).toFixed(1), 0)}s`}</div>
+        <div className="text-3xl">{`${Math.max(((secondsPerQuestion - stopwatch)).toFixed(1), 0)}s`}</div>
         <div className="text-xl">{`Correct Streak: ${correct}`}</div>
         <div className="flex flex-row items-center gap-5">
             <div className="text-xl">{`Max Streak: ${maxStreak}`}</div>

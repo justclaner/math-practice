da153c805b4aa0fa6a11d6679768e16a7616340d
= <justclaner@gmail.com>
2025-08-08 16:46:06 -0400

    renamed checkbox label

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import {Routes, Route} from 'react-router-dom';
 import Multiplication from './Pages/Multiplication.jsx';
+import Division from './Pages/Division.jsx';
 
 const App = () => {
   return (
      <Routes>
         <Route path='/multiplication' element={<Multiplication />}/>
+        <Route path='/division' element={<Division />}/>
      </Routes>
   )
 }
